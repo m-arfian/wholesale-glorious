f05dd07b4ba71a5bea3f83526e3e1ad97333c4b4
Muchammad Arfian <muchammad.arfian@gmail.com>
2014-07-30 10:26:32 +0700

    1.1

--- protected/modules/sim/views/pelanggan/index.php
+++ protected/modules/sim/views/pelanggan/index.php
@@ -19,63 +19,68 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<div class="row-fluid">
-    <div class="span9">
-        <div class="well well-white">
-            <div class="row-fluid">
-                <div class="span9">
-                    <h2 class="lead"><i class="icon-user"></i> &nbsp;Manajemen Pelanggan</h2>
-                </div>
-                <div class="span3">
-                    <?php echo CHtml::link('<i class="icon-plus"></i> Tambah data', array('create'), array('class'=>'btn pull-right')) ?>
+<div class="blue-block">
+    <div class="page-title">
+        <h3 class="pull-left"><i class="fa fa-truck"></i> Manajemen Pelanggan <span></span></h3>
+        <?php $this->renderPartial('/layouts/_breadcrumb') ?>
+        <div class="clearfix"></div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="page-content page-tables">
+
+        <div class="row" id="flashtop">
+            <?php echo Yii::app()->user->getFlash('info') ?>
+            <div class="col-md-12 col-sm-12 col-xs-12" style="display:none"></div>
+        </div>
+
+        <div class="widget">
+            <div class="search-form" style="display:none">
+                <?php $this->renderPartial('_search', array('model' => $model)) ?>
+            </div><!-- search-form -->
+            <div class="widget-head">
+                <?php echo CHtml::link('<i class="fa fa-search-plus"></i> Pencarian pelanggan', '#', array('class' => 'btn btn-xs btn-black search-button')); ?>
+                <?php echo CHtml::link('<i class="fa fa-plus"></i> Tambah pelanggan', array('create'), array('class' => 'btn btn-xs btn-success')) ?>
+            </div>
+            <div class="widget-body">
+                <div class="table-responsive">
+                    <?php
+                    $this->widget('zii.widgets.grid.CGridView', array(
+                        'id' => 'pelanggan-grid',
+                        'dataProvider' => $model->search(),
+                        //styling pagination
+                        'pager' => array(
+                            'header' => '',
+                            'selectedPageCssClass' => 'active',
+                            'hiddenPageCssClass' => 'disabled',
+                            'htmlOptions' => array('class' => ''),
+                        ),
+                        'pagerCssClass' => 'pagination',
+                        //end styling pagination
+                        'summaryText' => 'Menampilkan {start} - {end} dari {count} data pelanggan',
+                        'emptyText' => '<div class="alert alert-error">Tidak ada data pelanggan ditampilkan</div>',
+                        'showTableOnEmpty' => false,
+                        'itemsCssClass' => 'table table-bordered table-striped table-hover table-condensed',
+                        'columns' => array(
+                            'PELANGGAN_ID',
+                            'NAMA',
+                            'EMAIL',
+                            'HP',
+                            array(
+                                'name' => 'PELANGGAN_STATUS',
+                                'header' => 'Akun',
+                                'type' => 'StatusPelanggan',
+                                'value' => '$data->PELANGGAN_STATUS',
+                            ),
+                            array(
+                                'class' => 'MyCButtonColumn',
+                            ),
+                        ),
+                    ));
+                    ?>
                 </div>
             </div>
-            <?php echo CHtml::link('Pencarian', '#', array('class' => 'search-button')); ?>
-            <div class="search-form" style="display:none">
-                <?php
-                $this->renderPartial('_search', array(
-                    'model' => $model,
-                ));
-                ?>
-            </div><!-- search-form -->
-            <?php echo Yii::app()->user->getFlash('subinfo') ?>
-            <?php
-            $this->widget('zii.widgets.grid.CGridView', array(
-                'id' => 'pelanggan-grid',
-                'dataProvider' => $model->search(),
-                //styling pagination
-                'pager' => array(
-                    'header' => '',
-                    'selectedPageCssClass' => 'active',
-                    'hiddenPageCssClass' => 'disabled',
-                    'htmlOptions' => array('class' => ''),
-                ),
-                'pagerCssClass' => 'pagination',
-                //end styling pagination
-                'summaryText' => 'Menampilkan {start} - {end} dari {count} data pelanggan',
-                'emptyText' => '<div class="alert alert-error">Tidak ada data pelanggan ditampilkan</div>',
-                'showTableOnEmpty' => false,
-                'itemsCssClass' => 'table table-bordered table-striped table-hover table-condensed',
-                'columns' => array(
-                    'PELANGGAN_ID',
-                    'NAMA',
-                    'EMAIL',
-                    'HP',
-                    array(
-                        'name' => 'PELANGGAN_STATUS',
-                        'header' => 'Akun',
-                        'type' => 'StatusPelanggan',
-                        'value' => '$data->PELANGGAN_STATUS',
-                    ),
-                    array(
-                        'class' => 'MyCButtonColumn',
-                    ),
-                ),
-            ));
-            ?>
         </div>
-    </div>
-    <div class="span3">
-        <?php $this->renderPartial('/layouts/_rightside') ?>
     </div>
 </div>
--- protected/modules/sim/views/pelanggan/view.php
+++ protected/modules/sim/views/pelanggan/view.php
@@ -8,76 +8,90 @@ $this->breadcrumbs = array(
 );
 ?>
 
-<div class="row-fluid">
-    <div class="span9">
-        <div class="well well-white">
-            <div class="row-fluid">
-                <div class="span12">
-                    <h2 class="lead"><i class="icon-user"></i> &nbsp;Detail Pelanggan #<?php echo $model->NAMA ?></h2>
+<div class="blue-block">
+    <div class="page-title">
+        <h3 class="pull-left"><i class="fa fa-files-o"></i> Manajemen Pelanggan <span>Detail pelanggan #<?php echo $model->NAMA ?></span></h3>
+        <?php $this->renderPartial('/layouts/_breadcrumb') ?>
+        <div class="clearfix"></div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="page-content page-tables">
+        <div id="flashtop">
+            <?php echo Yii::app()->user->getFlash('info') ?>
+            <div class="col-md-12 col-sm-12 col-xs-12" style="display:none"></div>
+        </div>
+
+        <div class="widget">
+            <div class="widget-head">
+                <?php echo CHtml::link('<i class="fa fa-plus"></i>', array('create'), array('class' => 'btn btn-sm tip', 'data-toggle' => 'tooltip', 'title' => 'Tambah pelanggan')) ?>
+                <?php echo CHtml::link('<i class="fa fa-pencil"></i>', array('update', 'id' => $model->PELANGGAN_ID), array('class' => 'btn btn-sm tip', 'data-toggle' => 'tooltip', 'title' => 'Ubah pelanggan')) ?>
+            </div>
+            <div class="widget-body">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+
+                        <?php
+                        $this->widget('zii.widgets.CDetailView', array(
+                            'data' => $model,
+                            'htmlOptions' => array(
+                                'class' => 'table table-bordered table-striped table-view',
+                            ),
+                            'attributes' => array(
+                                'PELANGGAN_ID',
+                                'NAMA',
+                                'EMAIL',
+                                'HP',
+                                array(
+                                    'name' => 'KELAMIN',
+                                    'type' => 'Kelamin',
+                                    'value' => $model->KELAMIN,
+                                ),
+                                array(
+                                    'name' => 'PELANGGAN_STATUS',
+                                    'type' => 'StatusPelanggan',
+                                    'value' => $model->PELANGGAN_STATUS,
+                                ),
+                            ),
+                        ));
+                        ?>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <?php
+                        if ($model->PELANGGAN_STATUS == Pelanggan::PUNYA_AKUN &&
+                                $model->registered->PELANGGAN_ID == $model->PELANGGAN_ID) {
+
+                            $this->widget('zii.widgets.CDetailView', array(
+                                'data' => $model->registered,
+                                'htmlOptions' => array(
+                                    'class' => 'table table-bordered table-striped table-view',
+                                ),
+                                'attributes' => array(
+                                    'REGISTERED_ID',
+                                    'USERNAME',
+                                    array(
+                                        'name' => 'MEMBER_SINCE',
+                                        'type' => 'tanggalwaktu',
+                                        'value' => $model->registered->MEMBER_SINCE,
+                                    ),
+                                    array(
+                                        'name' => 'LAST_LOGIN',
+                                        'type' => 'tanggalwaktu',
+                                        'value' => $model->registered->LAST_LOGIN,
+                                    ),
+                                    array(
+                                        'name' => 'STATUS',
+                                        'type' => 'StatusAktif',
+                                        'value' => $model->registered->STATUS,
+                                    ),
+                                ),
+                            ));
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
-            <?php echo Yii::app()->user->getFlash('subinfo') ?>
-
-            <?php
-            if ($model->PELANGGAN_STATUS == Pelanggan::PUNYA_AKUN &&
-                    $model->registered->PELANGGAN_ID == $model->PELANGGAN_ID
-            ) {
-                $this->widget('zii.widgets.CDetailView', array(
-                    'data' => $model->registered,
-                    'htmlOptions' => array(
-                        'class' => 'table table-bordered table-striped table-view',
-                    ),
-                    'attributes' => array(
-                        'REGISTERED_ID',
-                        'USERNAME',
-                        array(
-                            'name' => 'MEMBER_SINCE',
-                            'type' => 'DateFormat',
-                            'value' => $model->registered->MEMBER_SINCE,
-                        ),
-                        array(
-                            'name' => 'LAST_LOGIN',
-                            'type' => 'DateFormat',
-                            'value' => $model->registered->LAST_LOGIN,
-                        ),
-                        array(
-                            'name' => 'STATUS',
-                            'type' => 'StatusAktif',
-                            'value' => $model->registered->STATUS,
-                        ),
-                    ),
-                ));
-                echo '<br>';
-            }
-            ?>
-
-            <?php
-            $this->widget('zii.widgets.CDetailView', array(
-                'data' => $model,
-                'htmlOptions' => array(
-                    'class' => 'table table-bordered table-striped table-view',
-                ),
-                'attributes' => array(
-                    'PELANGGAN_ID',
-                    'NAMA',
-                    'EMAIL',
-                    'HP',
-                    array(
-                        'name' => 'KELAMIN',
-                        'type' => 'Kelamin',
-                        'value' => $model->KELAMIN,
-                    ),
-                    array(
-                        'name' => 'PELANGGAN_STATUS',
-                        'type' => 'StatusPelanggan',
-                        'value' => $model->PELANGGAN_STATUS,
-                    ),
-                ),
-            ));
-            ?>
         </div>
-    </div>
-    <div class="span3">
-        <?php $this->renderPartial('/layouts/_rightside') ?>
     </div>
 </div>
